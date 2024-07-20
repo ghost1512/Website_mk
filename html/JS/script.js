4b005bdab508f463e9a824bea68a8ccd2f82b949
BaoHG <33833362+ghost1512@users.noreply.github.com>
2024-07-20 08:21:26 +0700

    first
    first

---
+++ html/JS/script.js
@@ -0,0 +1,38 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-item-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const productsMenu = document.getElementById('products-menu');
+    const dropdown = productsMenu.querySelector('.dropdown');
+
+    productsMenu.addEventListener('click', () => {
+        dropdown.classList.toggle('show');
+    });
+});
